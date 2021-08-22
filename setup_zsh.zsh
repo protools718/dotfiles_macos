5c70923da02d33ecab4a2dab5c688f191750dc8c
Ed Branch <protools718@users.noreply.github.com>
2021-08-22 16:43:59 -0400

    Symlink sh to zsh

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -8,14 +8,21 @@ echo "\n<<< Starting ZSH  Setup >>>\n"
 if grep -Fxq '/usr/local/bin/zsh' '/etc/shells';then
     echo  '/usr/local/bin/zsh alreade exists in /etc/shells'
 else
-echo "Enter superuser (sudo) password to edit /etc/shells"
-echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
+    echo "Enter superuser (sudo) password to edit /etc/shells"
+    echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
 fi
 
 if [ "$SHELL" = '/usr/local/bin/zsh ']; then
     echo 'SHELL is already /usr/local/bin/zsh'
 else
-echo "Enter user password to change login shell"
-chsh -s '/usr/local/bin/zsh'
+    echo "Enter user password to change login shell"
+    chsh -s '/usr/local/bin/zsh'
+fi
+
+if sh --version | grep -q zsh; then
+    echo '/private/var/select/sh is already linked to /bin/zsh'
+else
+    echo "Enter superuser (sudo) password to symlink sh to zsh"
+    sudo ln -sfv /bin/zsh /private/var/select/sh
 fi
 
